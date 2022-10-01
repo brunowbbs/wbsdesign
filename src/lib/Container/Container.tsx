07576534620ec6c6e78470bca36fe0b010fb6676
Wesley Bruno <engwesleybruno@gmail.com>
2022-10-01 13:40:16 -0300

    button

---
+++ src/lib/Container/Container.tsx
@@ -0,0 +1,18 @@
+import { Card } from "antd";
+import React from "react";
+
+const Container: React.FC = () => (
+  <>
+    <Card
+      title="Default size card"
+      extra={<a href="#">More</a>}
+      style={{ width: 300 }}
+    >
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+  </>
+);
+
+export default Container;
